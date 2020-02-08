4d24310a4032acf306e1255a72466df7fed6fa3b
cruim <hedgehog147@gmail.com>
2020-02-08 16:28:49 +0300

    longest_substring

---
+++ longest_substring.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        best = ''
+        for index, i in enumerate(s):
+            cur = ''
+            for j in s[index:]:
+                if j not in cur:
+                    cur += j
+                    best = best if len(best) >= len(cur) else cur
+                    continue
+                else:
+                    break
+        return len(best)
