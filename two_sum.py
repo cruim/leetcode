00558f6628f74081058124881797908cd0659f99
cruim <hedgehog147@gmail.com>
2020-02-08 16:25:12 +0300

    two_sum

---
+++ two_sum.py
@@ -0,0 +1,10 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for index, item in enumerate(nums):
+            for i in nums[index+1::]:
+                res = item + i
+                if res == target:
+                    return [nums.index(item), nums.index(i if item != i else i,index+1)]
